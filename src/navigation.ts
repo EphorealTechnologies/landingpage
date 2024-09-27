830d96105f889ed8b1079c37e83870bc1c501f05
EphorealTechnologies <tim22442244@gmail.com>
2024-09-28 03:10:05 +0530

    major changes

--- src/navigation.ts
+++ src/navigation.ts
@@ -5,63 +5,66 @@ export const headerData = {
     {
       text: 'Home',
       href: getPermalink('/'),
-      // links: [
-      //   {
-      //     text: 'SaaS',
-      //     href: getPermalink('/homes/saas'),
-      //   },
-      //   {
-      //     text: 'Startup',
-      //     href: getPermalink('/homes/startup'),
-      //   },
-      //   {
-      //     text: 'Mobile App',
-      //     href: getPermalink('/homes/mobile-app'),
-      //   },
-      //   {
-      //     text: 'Personal',
-      //     href: getPermalink('/homes/personal'),
-      //   },
-      // ],
+     
     },
     {
       text: 'Services',
       links: [
         {
           text: 'Strategic Consult',
-          href: getPermalink('/Consult'),
+          href: getPermalink('/homes/Consult'),
         },
         {
           text: 'GenAI',
-          href: getPermalink('/GenAI'),
+          href: getPermalink('/homes/GenAI'),
         },
         {
           text: 'Data Engineering',
-          href: getPermalink('/DataEngineering'),
+          href: getPermalink('/homes/DataEngineering'),
         },
         {
           text: 'Custom Machine Learning',
-          href: getPermalink('/ML'),
+          href: getPermalink('/homes/ML'),
         },
         {
           text: 'Computer Vision',
-          href: getPermalink('/ComputerVision'),
+          href: getPermalink('/homes/ComputerVision'),
         },
         {
           text: 'IOT',
-          href: getPermalink('/IOT'),
+          href: getPermalink('/homes/IOT'),
         },
-        // {
-        //   text: 'Terms',
-        //   href: getPermalink('/terms'),
-        // },
-        // {
-        //   text: 'Privacy policy',
-        //   href: getPermalink('/privacy'),
-        // },
+        {
+          text: 'MLOPS',
+          href: getPermalink('/homes/MLOPS'),
+        },
       ],
     },
-
+    {
+      text: 'Industries',
+      links: [
+        {
+          text: 'Healthcare',
+          href: getPermalink('/homes/Healthcare'),
+        },
+        {
+          text: 'Manufacturing',
+          href: getPermalink('/homes/Manufacturing'),
+        },
+        {
+          text: 'Retail',
+          href: getPermalink('/homes/Retail'),
+        },
+        {
+          text: 'banking and finance',
+          href: getPermalink('/homes/banking_finance'),
+        },
+        {
+          text: 'Computer Vision',
+          href: getPermalink('/homes/Marketing'),
+        },
+      ],
+    },
     {
       text: 'AboutUs',
       href: getPermalink('/about'),
@@ -70,118 +73,66 @@ export const headerData = {
       text: 'Contact',
       href: getPermalink('/contact'),
     },
-    // {
-    //   text: 'Landing',
-    //   links: [
-    //     {
-    //       text: 'Lead Generation',
-    //       href: getPermalink('/landing/lead-generation'),
-    //     },
-    //     {
-    //       text: 'Long-form Sales',
-    //       href: getPermalink('/landing/sales'),
-    //     },
-    //     {
-    //       text: 'Click-Through',
-    //       href: getPermalink('/landing/click-through'),
-    //     },
-    //     {
-    //       text: 'Product Details (or Services)',
-    //       href: getPermalink('/landing/product'),
-    //     },
-    //     {
-    //       text: 'Coming Soon or Pre-Launch',
-    //       href: getPermalink('/landing/pre-launch'),
-    //     },
-    //     {
-    //       text: 'Subscription',
-    //       href: getPermalink('/landing/subscription'),
-    //     },
-    //   ],
-    // },
+   
     {
       text: 'Blog',
       href: getBlogPermalink(),
-      // links: [
-      //   {
-      //     text: 'Blog List',
-      //     href: getBlogPermalink(),
-      //   },
-        // {
-        //   text: 'Article',
-        //   href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        // },
-        // {
-        //   text: 'Article (with MDX)',
-        //   href: getPermalink('markdown-elements-demo-post', 'post'),
-        // },
-        // {
-        //   text: 'Category Page',
-        //   href: getPermalink('tutorials', 'category'),
-        // },
-        // {
-        //   text: 'Tag Page',
-        //   href: getPermalink('astro', 'tag'),
-        // },
-      // ],
+     
     },
-    // {
-    //   text: 'Widgets',
-    //   href: '#',
-    // },
+    
   ],
-  // actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'Services',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Strategic consult', href: '/homes/Consult' },
+        { text: 'GenAI', href: '/homes/GenAI' },
+        { text: 'Data Engineering', href: '/homes/DataEngineering' },
+        { text: 'Custom Machine Learning', href: '/homes/ML' },
+        { text: 'Computer Vision', href: '/homes/ComputerVision' },
+        { text: 'IOT', href: '/homes/IOT' },
+        { text: 'MLOPS', href: '/homes/MLOPS' },
       ],
     },
+    // {
+    //   title: 'Platform',
+    //   links: [
+    //     { text: 'Developer API', href: '#' },
+    //     { text: 'Partners', href: '#' },
+    //     { text: 'Atom', href: '#' },
+    //     { text: 'Electron', href: '#' },
+    //     { text: 'AstroWind Desktop', href: '#' },
+    //   ],
+    // },
     {
-      title: 'Platform',
+      title: 'Contact Us',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
+        { text: 'Email:contact@ephoreal.com', href: 'mailto:contact@ephoreal.com' },
+        { text: 'Book a meeting', href: getPermalink('/contact') },
+        { text: 'Address: 2nd floor, 14, Rajaji Street, Sudhana Nagar, Nainarmandapam, Puducherry, India-605004'},
+        // { text: 'Skills', href: '#' },
+        // { text: 'Status', href: '#' },
       ],
     },
     {
       title: 'Company',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'About', href: '/about' },
+        { text: 'Blog', href: '/blog' },
+        { text:"LinkedIn", icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/ephoreal-technologies/' },
+        { text:"X",icon: 'tabler:brand-X', href: 'https://www.X.com/EphorealTech' },
+        // { text: 'X', href: '#' },
+        // { text: 'Inclusion', href: '#' },
+        // { text: 'Social Impact', href: '#' },
+        // { text: 'Shop', href: '#' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
+    // { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
@@ -191,6 +142,6 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="src/assets/images/logo.svg" alt="onWidget logo" loading="lazy"></img>
-    Ephoreal Technologies (OPC) PVT LMT @All rights reserved.
+    ©2024 by Ephoreal Technologies. All Rights Reserved.
   `,
 };
